f88e27051021d985d53d7365a0763e7075388a23
dhpcode <davrsirius@gmail.com>
2025-11-12 16:03:01 +0500

    project completed

---
+++ js/forecast.js
@@ -0,0 +1,17 @@
+const KEY = '96b947a45d33d7dc1c49af3203966408'
+// bu mening kalitim. Agar o'zingiz olsangiz va ishlamasa buni ishlating.
+// Agar ishlamay qolsa unda 1 soatda so'rovlar soni limitdan oshib ketgan bo'ladi.
+// Aloqa: Telegram => @akror_web
+
+
+const getData = async (city) => {
+  const base = 'https://api.openweathermap.org/data/2.5/weather';
+  const query = `?q=${city}&units=metric&appid=${KEY}`;
+  loader(true)
+  const request = await fetch(base + query);
+  const data = await request.json();
+  console.log(data)
+  loader(false)
+
+  return data;
+}
